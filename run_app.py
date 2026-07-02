05bb9d97f466c36c579a0f7b71f58579572e46f1
D-one33 <oned76983@gmail.com>
2026-07-03 01:06:52 +0530

    Add files via upload

---
+++ run_app.py
@@ -0,0 +1,7 @@
+import os
+import sys
+import streamlit.web.cli as stcli
+
+if __name__ == '__main__':
+    sys.argv = ["streamlit", "run", "app.py", "--global.developmentMode=false"]
+    sys.exit(stcli.main())
